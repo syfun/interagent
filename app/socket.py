2f586f71cc4d5bf457f571a054fb5b32ea6b6748
syfun <sunyu418@gmail.com>
2020-06-05 20:43:49 +0800

    update event handle

--- app/socket.py
+++ app/socket.py
@@ -9,20 +9,19 @@ sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
 
 @sio.on('waitTouch')
 async def wait_touch(sid, data):
-    await sio.emit('waitTouch', data)
+    await sio.emit('waitTouch', 'waitTouch')
 
 
 @sio.on('touchStart')
 async def touch_start(sid, data):
-    await sio.emit('touchStart', data)
+    await sio.emit('touchStart', 'touchStart')
 
 
 @sio.on('touchEnd')
 async def touch_end(sid, data):
-    await sio.emit('touchEnd', data)
+    await sio.emit('touchEnd', 'touchEnd')
 
 
 @sio.on('test')
 async def test(sid, data):
-    await sio.emit('test', data)
-
+    await sio.emit('test', 'test')
